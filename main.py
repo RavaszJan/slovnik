6adbf7f29dbd815d299ae03fb4b9dfad1e9ffaa8
RavaszJan <156021333+RavaszJan@users.noreply.github.com>
2024-02-06 10:55:20 +0100

    final

--- main.py
+++ main.py
@@ -1,35 +1,56 @@
+import pickle
 
-zoznam={"CR":"Praha","SR":"Bratislava","Polsko":"Warsava"}
+zoznam = {"CR": "Praha", "SR": "Bratislava", "Polsko": "Warsava", "Madarsko": "Budapest"}
 
 
 def pridaj(zoznam):
-    country=input("Zadaj nazov statu pre pridanie:")
-    city=input("Zadaj nazov hlavneho mesta:")
-    zoznam.update([(country,city)])
     print(zoznam)
+    country = input("Zadaj nazov statu:")
+    city = input("Zadaj nazov hlavneho mesta:")
+    zoznam.update([(country, city)])
+    print(zoznam)
+
 
 def odober(zoznam):
     print(zoznam)
-    country=input("Zadaj nazov statu zo zoznamu na vymazanie:")
+    country = input("Zadaj nazov statu zo zoznamu na vymazanie:")
     del zoznam[country]
+    print("Stat:{} bol vymazany zo zoznamu".format(country))
     print(zoznam)
+
+
 def vyhladat(zoznam):
-    stat=input("Zadaj nazov statu pre vyhladavanie:")
+    stat = input("Zadaj nazov statu pre vyhladavanie:")
     if stat in zoznam:
         print(zoznam[stat])
     else:
         print("Stat sa nenasiel v zozname")
 
 
-
 def zmena(zoznam):
     print(zoznam)
-    stat=input("Zadaj stat, ktory sa zmeni:")
-    mesto=input("Zadaj nove city:")
-    zoznam[stat]=mesto
+    stat = input("Zadaj stat, ktory sa zmeni:")
+    mesto = input("Zadaj nove city:")
+    zoznam[stat] = mesto
     print(zoznam)
 
-pridaj(zoznam)
-odober(zoznam)
-vyhladat(zoznam)
-zmena(zoznam)
+
+def vloz_do_suboru(nazov_suboru):
+    with open(nazov_suboru, "wb") as file:
+        pickle.dump(zoznam, file)
+        print(zoznam)
+
+
+def vytvor_zo_suboru(nazov_suboru):
+    with open(nazov_suboru, "rb") as file:
+        return pickle.load(file)
+
+
+# pridaj(zoznam)
+# odober(zoznam)
+# vyhladat(zoznam)
+# zmena(zoznam)
+vloz_do_suboru("country.txt")
+print(vloz_do_suboru)
+obnov = vytvor_zo_suboru("country.txt")
+print(obnov)
